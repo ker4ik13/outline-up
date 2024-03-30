db1a2f10acf10eee9f3537f569c341fe11ba230b
ker4ik13 <kekustop228@mail.ru>
2024-03-31 00:19:41 +0300

    fix: styles

--- client/src/app/(default)/layout.tsx
+++ client/src/app/(default)/layout.tsx
@@ -1,7 +1,6 @@
 import iconWebp from "@/data/images/favicon/icon.webp";
 import { Footer, Nav } from "@/widgets/user";
 import type { Metadata } from "next";
-import "../styles";
 import "./styles";
 
 export const metadata: Metadata = {
